22ffb7e0480a2038b447342f0a20ef105d5ba390
babusharif875 <payelbegum96@gmail.com>
2021-03-17 20:18:09 +0600

    Switch-2

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-var letter = prompt("Enter")
-document.write(letter)
+var letter = prompt("Enter any letter : ")
+letter = letter.toLowerCase();
+
+switch(letter){
+    case"a":
+    case"E":
+    case"i":
+    case"o":
+    case"u":
+    document.write("Vowel")
+    break;
+   default:
+   document.write("Consonant")
+
+
+}
